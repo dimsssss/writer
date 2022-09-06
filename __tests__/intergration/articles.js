985af583339645cfd19e92af2336e869add12b5f
dimsssss <caruen@hanmail.net>
2022-09-06 21:08:46 +0900

    test: 게시글 수정 테스트

--- __tests__/intergration/articles.js
+++ __tests__/intergration/articles.js
@@ -24,4 +24,29 @@ describe("게시글 통합테스트 입니다", () => {
     expect(result.title).toEqual(data.title);
     expect(result.content).toEqual(data.content);
   });
+
+  test("본인 게시글이고 패스워드가 동일하다면 게시글을 수정할 수 있다", async () => {
+    // when
+    const data = require("../dummy.json").create.emojiArticle;
+    const updateData = require("../dummy.json").update.emojiArticle;
+    const { updateUserArticle } = require("../../article/articleService");
+    const { convertPasswordInArticle } = require("../../article/articleDecorator");
+    const { articles } = require("../../bin/database");
+    // then
+    const article = await articles.create(await convertPasswordInArticle(data), { raw: true });
+    const result = await updateUserArticle({ ...updateData, articleId: article.articleId });
+    expect(result).toEqual([1]);
+
+    const updateArticle = await articles.findByPk(article.sequenceId, { raw: true });
+
+    expect(updateArticle.userId).toEqual(updateData.userId);
+    expect(updateArticle.title).toEqual(updateData.title);
+    expect(updateArticle.content).toEqual(updateData.content);
+  });
+
+  test("본인 게시글이고 패스워드가 다르면 게시글을 수정할 수 없다", async () => {});
+
+  test("남의 게시글이고 패스워드가 같으면 게시글을 수정할 수 없다", async () => {});
+
+  test("남의 게시글이고 패스워드가 다르면 게시글을 수정할 수 없다", async () => {});
 });
