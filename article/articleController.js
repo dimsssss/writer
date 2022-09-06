e3c33ce13fe005d940cb47d136ff23a00698585a
dimsssss <caruen@hanmail.net>
2022-09-06 21:09:03 +0900

    feat: 게시글 수정 컨트롤러

--- article/articleController.js
+++ article/articleController.js
@@ -19,6 +19,23 @@ const postArticle = async (req, res) => {
   }
 };
 
+const updateArticle = async (req, res) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty()) {
+    return res.status(StatusCodes.BAD_REQUEST).send(error);
+  }
+
+  try {
+    const article = req.body;
+    const result = await postUserArticle(article);
+    return res.status(StatusCodes.OK).send(result);
+  } catch (err) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
+};
+
 module.exports = {
   postArticle,
+  updateArticle,
 };
