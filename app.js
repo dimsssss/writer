1e07749368f4e07403af780d691620279ed7d267
dimsssss <caruen@hanmail.net>
2022-09-06 12:56:11 +0900

    refactor: prettier적용

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 /* global process */
 
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const indexRouter = require('./sample/index')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./article/index");
 
-const swaggerUi = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser())
-app.use(logger('dev'))
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.use('/', indexRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger("dev"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", indexRouter);
 
-module.exports = app
+module.exports = app;
