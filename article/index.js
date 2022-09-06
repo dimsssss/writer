9e5d9797157a0c09f35ddf5d1d657f08fddaecce
dimsssss <caruen@hanmail.net>
2022-09-06 21:11:23 +0900

    feat: 라우터 추가

--- article/index.js
+++ article/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { validateArticle } = require("./articleValidator");
+const { validateArticle, updateArticleBody } = require("./articleValidator");
 const articleController = require("./articleController");
 
 router.post("/", validateArticle, articleController.postArticle);
+router.patch("/", updateArticleBody, articleController.updateArticle);
 
 module.exports = router;
