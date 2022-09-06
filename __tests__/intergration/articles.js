89f489928cdb190a2b8e535e66ce0de8211e4d85
dimsssss <caruen@hanmail.net>
2022-09-07 00:20:59 +0900

    test: 게시글 삭제 테스트 추가

--- __tests__/intergration/articles.js
+++ __tests__/intergration/articles.js
@@ -6,7 +6,7 @@ describe("게시글 통합테스트 입니다", () => {
   });
 
   test("게시글을 생성할 수 있다", async () => {
-    const data = require("../dummy.json").create.article;
+    const data = require("./dummy.json").create.article;
     const { postUserArticle } = require("../../article/articleService");
     const result = await postUserArticle(data);
 
@@ -16,7 +16,7 @@ describe("게시글 통합테스트 입니다", () => {
   });
 
   test("게시글은 이모지가 포함될 수 있다", async () => {
-    const data = require("../dummy.json").create.emojiArticle;
+    const data = require("./dummy.json").create.emojiArticle;
     const { postUserArticle } = require("../../article/articleService");
     const result = await postUserArticle(data);
 
@@ -27,8 +27,8 @@ describe("게시글 통합테스트 입니다", () => {
 
   test("본인 게시글이고 패스워드가 동일하다면 게시글을 수정할 수 있다", async () => {
     // when
-    const data = require("../dummy.json").create.emojiArticle;
-    const updateData = require("../dummy.json").update.emojiArticle;
+    const data = require("./dummy.json").create.emojiArticle;
+    const updateData = require("./dummy.json").update.emojiArticle;
     const { updateUserArticle } = require("../../article/articleService");
     const { convertPasswordInArticle } = require("../../article/articleDecorator");
     const { articles } = require("../../bin/database");
@@ -44,9 +44,81 @@ describe("게시글 통합테스트 입니다", () => {
     expect(updateArticle.content).toEqual(updateData.content);
   });
 
-  test("본인 게시글이고 패스워드가 다르면 게시글을 수정할 수 없다", async () => {});
+  test("본인 게시글이고 패스워드가 같으면 게시글을 삭제할 수 있다", async () => {
+    // when
+    const data = require("./dummy.json").create.emojiArticle;
+    const { deleteUserArticle } = require("../../article/articleService");
+    const { convertPasswordInArticle } = require("../../article/articleDecorator");
+    const { articles } = require("../../bin/database");
+    // then
+    const article = await articles.create(await convertPasswordInArticle(data), { raw: true });
+    const deleteData = {
+      articleId: article.articleId,
+      userId: article.userId,
+      password: data.password,
+    };
 
-  test("남의 게시글이고 패스워드가 같으면 게시글을 수정할 수 없다", async () => {});
+    const deletedRowCount = await deleteUserArticle(deleteData);
+    expect(deletedRowCount).toEqual(1);
 
-  test("남의 게시글이고 패스워드가 다르면 게시글을 수정할 수 없다", async () => {});
+    const deletedArticle = await articles.findByPk(article.sequenceId);
+    expect(deletedArticle).toEqual(null);
+  });
+
+  test("본인 게시글이고 패스워드가 다르면 게시글을 삭제할 수 없다", async () => {
+    // when
+    const data = require("./dummy.json").create.emojiArticle;
+    const { deleteUserArticle } = require("../../article/articleService");
+    const { convertPasswordInArticle } = require("../../article/articleDecorator");
+    const { articles } = require("../../bin/database");
+    // then
+    const article = await articles.create(await convertPasswordInArticle(data), { raw: true });
+    // 다른 패스워드로 수정
+    const deleteData = {
+      articleId: article.articleId,
+      userId: article.userId,
+      password: "123455",
+    };
+
+    const UnValidResultException = require("../../article/exception/UnValidResultException");
+    await expect(deleteUserArticle(deleteData)).rejects.toThrowError(UnValidResultException);
+  });
+
+  test("남의 게시글이고 패스워드가 같으면 게시글을 삭제할 수 없다", async () => {
+    // when
+    const datas = require("./dummy.json").delete.articles;
+    const { deleteUserArticle } = require("../../article/articleService");
+    const { convertPasswordInArticle } = require("../../article/articleDecorator");
+    const { articles } = require("../../bin/database");
+    // then
+    const firstArticle = await articles.create(await convertPasswordInArticle(datas[0]), { raw: true });
+    const secondArticle = await articles.create(await convertPasswordInArticle(datas[1]), { raw: true });
+    const deleteData = {
+      articleId: secondArticle.articleId,
+      userId: firstArticle.userId,
+      password: datas[1].password,
+    };
+
+    const UnValidResultException = require("../../article/exception/UnValidResultException");
+    await expect(deleteUserArticle(deleteData)).rejects.toThrowError(UnValidResultException);
+  });
+
+  test("남의 게시글이고 패스워드가 다르면 게시글을 삭제할 수 없다", async () => {
+    // when
+    const datas = require("./dummy.json").delete.articles;
+    const { deleteUserArticle } = require("../../article/articleService");
+    const { convertPasswordInArticle } = require("../../article/articleDecorator");
+    const { articles } = require("../../bin/database");
+    // then
+    const firstArticle = await articles.create(await convertPasswordInArticle(datas[0]), { raw: true });
+    const secondArticle = await articles.create(await convertPasswordInArticle(datas[1]), { raw: true });
+    const deleteData = {
+      articleId: secondArticle.articleId,
+      userId: firstArticle.userId,
+      password: "abcdefg",
+    };
+
+    const UnValidResultException = require("../../article/exception/UnValidResultException");
+    await expect(deleteUserArticle(deleteData)).rejects.toThrowError(UnValidResultException);
+  });
 });
