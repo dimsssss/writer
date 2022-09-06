5e5f35d8b59e6a055210fc0ec794eae5c16d88a5
dimsssss <caruen@hanmail.net>
2022-09-06 21:11:15 +0900

    feat: update validator

--- article/articleValidator.js
+++ article/articleValidator.js
@@ -15,6 +15,22 @@ const validateArticle = [
   }),
 ];
 
+const updateArticleBody = [
+  body("title").isLength({ min: 1, max: 20 }),
+  body("content").isLength({ min: 1, max: 200 }),
+  body("articleId").isUUID(uuidVersion),
+  body("userId").isUUID(uuidVersion),
+  body("password").isStrongPassword({
+    minLength: 6,
+    minLowercase: 0,
+    minUppercase: 0,
+    minNumbers: 1,
+    minSymbols: 0,
+    returnScore: false,
+  }),
+];
+
 module.exports = {
   validateArticle,
+  updateArticleBody,
 };
