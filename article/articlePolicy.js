43cd121d378571fa1c6cc50ab07892a03c05ab0f
dimsssss <caruen@hanmail.net>
2022-09-07 15:26:56 +0900

    feat: 게시글 수정 정책 추가

--- article/articlePolicy.js
+++ article/articlePolicy.js
@@ -3,12 +3,21 @@ const { isEqualPassword } = require("../utils/bcrypt");
 
 const deletePolicy = async (articleInput, article) => {
   if (articleInput.userId !== article.userId) {
-    throw new UnValidResultException();
-    // "게시글 작성자가 다릅니다"
+    throw new UnValidResultException("게시글 작성자가 다릅니다");
   } else if (!(await isEqualPassword(articleInput.password, article.password))) {
-    throw new UnValidResultException();
-    // "게시글 패스워드가 다릅니다"
+    throw new UnValidResultException("게시글 패스워드가 다릅니다");
   }
 };
 
-module.exports = { deletePolicy };
+const updatePolicy = async (articleInput, article) => {
+  console.log(articleInput, article);
+  if (articleInput.userId !== article.userId) {
+    throw new UnValidResultException("게시글 작성자가 다릅니다");
+  } else if (!(await isEqualPassword(articleInput.password, article.password))) {
+    throw new UnValidResultException("게시글 패스워드가 다릅니다");
+  } else if (articleInput.weather) {
+    throw new UnValidResultException("게시글 작성 때의 날씨는 수정할 수 없습니다.");
+  }
+};
+
+module.exports = { deletePolicy, updatePolicy };
