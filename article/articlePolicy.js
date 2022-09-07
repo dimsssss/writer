eca508a0303acbf9f841393f56d8800959201904
dimsssss <caruen@hanmail.net>
2022-09-07 16:24:54 +0900

    fix: 날씨 비교 조건 수정

--- article/articlePolicy.js
+++ article/articlePolicy.js
@@ -10,12 +10,11 @@ const deletePolicy = async (articleInput, article) => {
 };
 
 const updatePolicy = async (articleInput, article) => {
-  console.log(articleInput, article);
   if (articleInput.userId !== article.userId) {
     throw new UnValidResultException("게시글 작성자가 다릅니다");
   } else if (!(await isEqualPassword(articleInput.password, article.password))) {
     throw new UnValidResultException("게시글 패스워드가 다릅니다");
-  } else if (articleInput.weather) {
+  } else if (articleInput.weather !== article.weather) {
     throw new UnValidResultException("게시글 작성 때의 날씨는 수정할 수 없습니다.");
   }
 };
